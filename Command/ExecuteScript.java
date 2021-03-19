8e84b14730efb45d25af38856fc79bf7c9abca14
LiaoYihong-1 <74876152+LiaoYihong-1@users.noreply.github.com>
2021-03-19 12:26:54 +0300

    Add files via upload

--- Command/ExecuteScript.java
+++ Command/ExecuteScript.java
@@ -6,6 +6,7 @@ import java.io.IOException;
  * command ExecuteScript
  */
 public class ExecuteScript extends AbstractCommand {
+    private int time=0;
     public ExecuteScript(){
         this.name="ExecuteScript";
         this.help=" read and execute a script from the specified file. The script contains commands in the same form in which the user enters them interactively.";
@@ -13,17 +14,19 @@ public class ExecuteScript extends AbstractCommand {
 
     /**
      * when length of parameter bigger than 2 or smaller than 1,throw ParaInapproException
-     * {@link CommandManager#executeExecuteScript(String, CommandManager)}
+     * {@link CommandManager#executeExecuteScript(String, CommandManager,String )}
      * @param commandManager
      * @param args
      * @throws IOException
      * @throws ParaInapproException
      */
-    public void execute(CommandManager commandManager,String args[]) throws IOException,ParaInapproException{
-        if(args.length>2||args.length==1){
-            throw new ParaInapproException("this command only and must accepts one parameter\n");
-        }else {
-            commandManager.executeExecuteScript(args[1],commandManager);
+    public void execute(CommandManager commandManager,String args[],String Saver) throws IOException,ParaInapproException {
+        if (time == 0) {
+            if (args.length > 2 || args.length == 1) {
+                throw new ParaInapproException("this command only and must accepts one parameter\n");
+            } else {
+                commandManager.executeExecuteScript(args[1], commandManager,Saver);
+            }
         }
     }
 }
